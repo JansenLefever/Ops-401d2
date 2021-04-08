6df00311afb96a8a299b1a344698eb83f58123f5
JansenLefever <jansenlefever@gmail.com>
2021-04-08 04:27:17 +0000

    Send email alerts if network connections goes down.

---
+++ ops03.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+# Author: Jansen Lefever
+# Class: 401D2 Ops Challenge 3
+# Purpose: Email failed connection alert 
+
+import smtplib, os, time, datetime
+
+fromaddr = 'thejanman19@gmail.com'  
+toaddrs  = 'jansenlefever@gmail.com'   
+
+username = 'thejanman19'  
+password = 'Spamthis'
+
+addr = input("What Ip Address would you like to ping:" )
+
+now = datetime.datetime.now()
+ping = os.system("ping -c 1 " + str(addr))
+
+def email_up():
+    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    server.ehlo()
+    (server.login(username, password))
+
+    msg = "Network connection to" + str(addr) + " " + "is back up \n"
+    server.sendmail(fromaddr, toaddrs, msg)
+    server.quit()
+
+def email_down():
+    server = smtplib.SMTP_SSL("smtp.gmail.com", 465)
+    server.ehlo()
+    (server.login(username, password))
+
+    msg = "Network connection to" + str(addr) + " " + "is down \n"
+    server.sendmail(fromaddr, toaddrs, msg)
+    server.quit()
+
+while True:
+        print()
+        time.sleep( 1 )
+        if ping == 0:
+            print((str(now)) + " " + "Network Active to " + str(addr))
+        if ping != 0:
+            print((str(now)) + " " + "Connection Failed to " + str(addr))
+            email_down()
+            if ping == 0:
+                print((str(now)) + " " + "Network Active to " + str(addr))
+                email_up()
+        time.sleep( 1 )

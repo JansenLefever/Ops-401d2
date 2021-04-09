3ea0bf66eece39c4905a9ab6e22913b218e658e7
Jansen Lefever <71102930+JansenLefever@users.noreply.github.com>
2021-04-08 19:32:56 -0700

    Update passwordenabled.ps1

--- passwordenabled.ps1
+++ passwordenabled.ps1
@@ -2,3 +2,6 @@ secedit /export /cfg c:\secpol.cfg
 (GC C:\secpol.cfg) -Replace "PasswordComplexity = 0","PasswordComplexity = 1" | Out-File C:\secpol.cfg
 secedit /configure /db c:\windows\security\local.sdb /cfg c:\secpol.cfg /areas SECURITYPOLICY
 Remove-Item C:\secpol.cfg -Force
+
+
+# https://www.reddit.com/r/PowerShell/comments/6mebpt/enforce_password_complexity_via_powershell/

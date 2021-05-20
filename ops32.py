e23d27268d0dae73a7404af96084a375469031d3
JansenLefever <jansenlefever@gmail.com>
2021-05-20 04:09:11 +0000

    it work now

--- ops32.py
+++ ops32.py
@@ -1,38 +1,31 @@
 #! /usr/bin/env python3
 
-import os, hashlib
+import os, hashlib, time, datetime
 
-def linux():
-    fil = input("Name a file: \n")
-    dirs = input("Name a Directory: \n")
-    os.system(f"ls {dirs}")
-    os.system(f"find {dirs} -name {fil}")
+def timestamp():
+    now = datetime.datetime.now()
+    timestamp = now.strftime("%m-%d-%y %H:%M:%S:%f")
+    return str(timestamp)
+
+def hash_it(datapath):
     md5_hash = hashlib.md5()
-    with open(fil,"rb") as f:
+    with open(datapath,"rb") as f:
         # Read and update hash in chunks of 4K
         for byte_block in iter(lambda: f.read(4096),b""):
             md5_hash.update(byte_block)
         print(md5_hash.hexdigest())
+     
+
+def linux():
+    filepath = input("Input absolute directory path: \n")
+    for root, dirs, files in os.walk(filepath):
+    # For each hit, concatenate the current directory pathing to left of result
+        for name in files:
+            datapath = (os.path.join(root, name))
+            time = timestamp()
+            print(time)
+            hash_it(datapath)
+            
     
-
-def windows():
-    fil = input("Name a file: \n")
-    dirs = input("Name a Directory: \n")
-    os.system(f"dir Images /AD /b /s {dirs} \\ {fil}").read()
-    with open(fil,"rb") as f:
-        # Read and update hash in chunks of 4K
-        for byte_block in iter(lambda: f.read(4096),b""):
-            md5_hash.update(byte_block)
-        print(md5_hash.hexdigest())    
-
-op = input(" 1) Linux 2) Windows 3) Exit \n")
-
-while True:
-    if (op == '1'):
-        linux()
-    elif (op == '2'):
-        windows()
-    elif (op == '3'):
-        break
-    else:
-        print("Incorrect Input")
+    
+linux()
